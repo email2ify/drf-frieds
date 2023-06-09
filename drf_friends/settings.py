eb7299051a4601d93d0e05730ed41adf48fa2bad
email2ify <email2ify@yahoo.com>
2023-06-09 16:06:36 +0000

    made corrections of deployment settings

--- drf_friends/settings.py
+++ drf_friends/settings.py
@@ -63,14 +63,14 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEBUG' in os.environ
+DEBUG = 'DEV' in os.environ
 
 #DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = [
     os.environ.get('ALLOWED_HOST'),
     
-    '8000-email2ify-drffriends-sgaznvkvhch.ws-eu99.gitpod.io',
+    '8000-email2ify-drffriends-ruwdiwwigr2.ws-eu99.gitpod.io',
     "localhost"
     
 ]
