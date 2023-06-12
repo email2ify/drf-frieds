6742fecde747f185239d3d9bc3802e46411746c9
email2ify <>
2023-06-12 17:59:12 +0000

    update setting

--- drf_friends/settings.py
+++ drf_friends/settings.py
@@ -63,16 +63,17 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEV' in os.environ
+DEBUG = 'DEBUG' in os.environ
 
 # DEBUG = 'DEBUG' in os.environ
 
 ALLOWED_HOSTS = [
     os.environ.get('ALLOWED_HOST'),
+
     '8000-email2ify-drf-friends-blqrkgnn9w.us2.codeanyapp.com',
     "localhost"
-]
 
+]
 if 'CLIENT_ORIGIN' in os.environ:
     CORS_ALLOWED_ORIGINS = [
         os.environ.get('CLIENT_ORIGIN')
