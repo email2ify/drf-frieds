c1d33cf7cec03a51a0182508b68eb00437439e17
email2ify <>
2023-06-24 21:17:00 +0000

    add logout route

--- drf_friends/urls.py
+++ drf_friends/urls.py
@@ -1,4 +1,4 @@
-"""drf_friends URL Configuration
+"""drf_api URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -20,15 +20,16 @@ from .views import root_route, logout_route
 urlpatterns = [
     path('', root_route),
     path('admin/', admin.site.urls),
-
     path('api-auth/', include('rest_framework.urls')),
+    # our logout route has to be above the default one to be matched first
+    path('dj-rest-auth/logout/', logout_route),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path(
         'dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')
     ),
-
     path('', include('profiles.urls')),
     path('', include('posts.urls')),
     path('', include('comments.urls')),
     path('', include('likes.urls')),
+   
 ]
