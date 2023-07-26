6955a03ad34d129e2eb5f4538352cc3d07067cd9
email2ify <email2ify@yahoo.com>
2023-07-26 17:18:39 +0000

    updated settings with debug false

--- drf_friends/settings.py
+++ drf_friends/settings.py
@@ -66,7 +66,8 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEV' in os.environ
+#DEBUG = 'DEV' in os.environ
+DEBUG = False
 
 ALLOWED_HOSTS = [
     os.environ.get('ALLOWED_HOST'),
